8a83e7e3ad88457193111aefc2b0f019bf5083bc
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-06-20 12:23:37 +0900

    Particle関連のフレームワークを追加

---
+++ Framework/BaseParticle.h
@@ -0,0 +1,38 @@
+//=====================================
+//
+//ベースパーティクルヘッダ[BaseParticle.h]
+//Author:GP12B332 21 立花雄太
+//
+//=====================================
+#ifndef _BASEPARTICLE_H_
+#define _BASEPARTICLE_H_
+
+#include "../main.h"
+
+/**************************************
+マクロ定義
+***************************************/
+
+/**************************************
+クラス定義
+***************************************/
+class BaseParticle
+{
+public:
+	BaseParticle();
+	virtual ~BaseParticle();
+
+	virtual void Init() = 0;
+	virtual void Uninit() = 0;
+	virtual void Update() = 0;
+	bool IsActive();
+
+	Transform transform;
+
+protected:
+	bool active;
+	int cntFrame;
+	int lifeFrame;
+};
+
+#endif

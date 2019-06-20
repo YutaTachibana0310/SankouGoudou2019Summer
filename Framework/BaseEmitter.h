e54ffbe4f6627be76aa9c7f768c5a1460a911f4d
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-06-20 12:44:29 +0900

    Emitterのフレームワーク作成

--- Framework/BaseEmitter.h
+++ Framework/BaseEmitter.h
@@ -1,1 +1,39 @@
-#pragma once
+//=====================================
+//
+//テンプレートヘッダ[BaseEmitter.h]
+//Author:GP12B332 21 立花雄太
+//
+//=====================================
+#ifndef _BASEEMITTER_H_
+#define _BASEEMITTER_H_
+
+#include "../main.h"
+
+/**************************************
+マクロ定義
+***************************************/
+
+/**************************************
+クラス定義
+***************************************/
+class BaseEmitter
+{
+public:
+	BaseEmitter();
+	virtual ~BaseEmitter();
+
+	virtual void Init() = 0;
+	virtual void Uninit() = 0;
+	virtual void Update() = 0;
+
+	bool IsActive();
+
+	Transform transform;
+
+protected:
+	bool active;
+	int cntFrame;
+	int duration;
+};
+
+#endif

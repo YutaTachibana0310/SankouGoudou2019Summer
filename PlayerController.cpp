fbdf0465c7d3ee515c3b6f5dca9021ebe012aad3
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-06-28 11:12:00 +0900

    衝突判定のテスト用処理をマスク

--- PlayerController.cpp
+++ PlayerController.cpp
@@ -21,7 +21,7 @@ using namespace std;
 #define MOVETARGET_LENGTH				(6)
 #define PLAYER_DISTANCE_FROM_CAMERA		(150.0f)
 
-#define USE_COLLIDER_TEST
+//#define USE_COLLIDER_TEST
 
 //*****************************************************************************
 // プロトタイプ宣言

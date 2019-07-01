05225868bf9a7dba2e5fa746f805da052ab8b0a1
YutaTachibana0310 <yuta.tachibana0310@gmail.com>
2019-07-01 09:50:43 +0900

    BaseEnemyModelを実装

---
+++ BaseEnemyModel.cpp
@@ -0,0 +1,78 @@
+//=====================================
+//
+//ベースエネミーモデル処理[BaseEnemyModel.cpp]
+//Author:GP12B332 21 立花雄太
+//
+//=====================================
+#include "BaseEnemyModel.h"
+
+/**************************************
+マクロ定義
+***************************************/
+
+/**************************************
+構造体定義
+***************************************/
+
+/**************************************
+コンストラクタ
+***************************************/
+BaseEnemyModel::BaseEnemyModel()
+{
+	collider = new TrailCollider(TrailColliderTag::Enemy);
+	collider->AddObserver(this);
+	collider->SetAddressZ(&pos.z);
+	active = false;
+}
+
+/**************************************
+デストラクタ
+***************************************/
+BaseEnemyModel::~BaseEnemyModel()
+{
+	SAFE_DELETE(collider);
+}
+
+/**************************************
+初期化処理
+***************************************/
+void BaseEnemyModel::Init(LineTrailModel model)
+{
+	this->model = model;
+	collider->SetTrailIndex(model);
+	cntFrame = 0;
+	active = true;
+}
+
+/**************************************
+終了処理
+***************************************/
+void BaseEnemyModel::Uninit()
+{
+	collider->RemoveFromCheckList();
+	active = false;
+}
+
+/**************************************
+更新処理
+***************************************/
+int BaseEnemyModel::Update()
+{
+
+}
+
+/**************************************
+描画処理
+***************************************/
+void BaseEnemyModel::Draw()
+{
+
+}
+
+/**************************************
+衝突判定通知レシーバー
+***************************************/
+void BaseEnemyModel::OnNotified()
+{
+
+}
---
+++ BaseEnemyModel.h
@@ -0,0 +1,55 @@
+//=====================================
+//
+//ベースエネミーモデルヘッダ[BaseEnemyModel.h]
+//Author:GP12B332 21 立花雄太
+//
+//=====================================
+#ifndef _BASEENEMYMODEL_H_
+#define _BASEENEMYMODEL_H_
+
+#include "main.h"
+#include "IStateMachine.h"
+#include "LineTrailModel.h"
+#include "Framework\BaseObserver.h"
+#include "TrailCollider.h"
+
+/**************************************
+マクロ定義
+***************************************/
+enum EnemyModelResult
+{
+	StateContinuous,
+	AttackTiming,
+	StateFinished,
+	ResultMax
+};
+
+/**************************************
+クラス定義
+***************************************/
+class BaseEnemyModel : BaseObserver
+{
+public:
+	friend class IStateMachine<BaseEnemyModel>;
+
+	BaseEnemyModel();
+	~BaseEnemyModel();
+
+	void Init(LineTrailModel model);
+	void Uninit();
+	int Update();
+	void Draw();
+
+	bool active;
+
+private:
+	LineTrailModel model;
+	TrailCollider *collider;
+	IStateMachine<BaseEnemyModel> *state;
+	D3DXVECTOR3 pos;
+	int cntFrame;
+
+	void OnNotified();
+};
+
+#endif

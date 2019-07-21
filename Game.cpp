b32e14fa846763df33aeb16c20d6cda827b548db
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-07-21 12:51:34 +0900

    commit

--- Game.cpp
+++ Game.cpp
@@ -56,7 +56,7 @@ static IStateScene* fsm[SceneMax];
 static SoundStateScene* ssm[SceneMax];
 
 //現在のシーン
-static Scene currentScene = SceneResult;
+static Scene currentScene = SceneGame;
 
 
 //シーンチェンジ用

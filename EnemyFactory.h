75be28638fea16cdb4bba1f1e374f85849aa0deb
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-08-01 16:36:56 +0900

    StraightEnemyFactoryを実装

--- EnemyFactory.h
+++ EnemyFactory.h
@@ -17,7 +17,11 @@
 ***************************************/
 
 /**************************************
-クラス定義
+EnemyFactoryクラス
+継承先で
+EnemyModel* Crate(picojson::object& data)
+void CreateGuide(picojson::object& data, EnemyGuideArrowController* controller)
+を実装する
 ***************************************/
 class EnemyFactory
 {

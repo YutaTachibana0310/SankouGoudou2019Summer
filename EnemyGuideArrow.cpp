a02b14f7383afcd3ac90ae0472af8c87b3426f97
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-08-02 11:32:39 +0900

    エネミーガイドの表示間隔とサイズを修正

--- EnemyGuideArrow.cpp
+++ EnemyGuideArrow.cpp
@@ -12,7 +12,7 @@
 É}ÉNÉçíËã`
 ***************************************/
 #define ENEMYGUIDEARROW_LIFEFRAME		(60)
-#define ENEMYGUIDEARROW_INTERVAL		(2)
+#define ENEMYGUIDEARROW_INTERVAL		(1)
 
 #define ENEMYGUIDEARROW_BASE_VECTOR		(&D3DXVECTOR3(0.0f, 1.0f, 0.0f))
 

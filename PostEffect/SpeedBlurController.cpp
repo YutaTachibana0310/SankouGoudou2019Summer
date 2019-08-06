e52845e86f1ba28e15e3a286e8f704ab1b998235
YutaTachibana <yuta.tachibana0310@gmail.com>
2019-08-06 13:30:47 +0900

    スピードブラーにパワー調整機能を追加

--- PostEffect/SpeedBlurController.cpp
+++ PostEffect/SpeedBlurController.cpp
@@ -6,6 +6,7 @@
 //=====================================
 #include "SpeedBlurFilter.h"
 #include "SpeedBlurController.h"
+#include "../Framework/Easing.h"
 
 /**************************************
 マクロ定義
@@ -17,12 +18,31 @@
 #endif // SPEEDBLURCTRL_USE_DEBUG
 
 /**************************************
-クラス定義
+マクロ定義
 ***************************************/
+#define SPEEDBLUR_TRANSITION_DURATION		(30)
 
 /**************************************
 グローバル変数
 ***************************************/
+
+/**************************************
+更新処理
+***************************************/
+void SpeedBlurController::Update()
+{
+	if (cntPower > 0)
+	{
+		cntPower--;
+
+		float t = 1.0f - (float)cntPower / SPEEDBLUR_TRANSITION_DURATION;
+		float power = Easing::EaseValue(t, startPower, endPower, EaseType::Linear);
+		speedBlur->SetPower(power);
+
+		if (cntPower == 0)
+			startPower = endPower;
+	}
+}
 
 /**************************************
 描画処理
@@ -32,11 +52,13 @@ void SpeedBlurController::Draw()
 #ifdef SPEEDBLURCTRL_USE_DEBUG
 	BeginDebugWindow("SpeedBlur");
 	static float startLen = 0.05f;
-	static float power = 5.0f;
+	static float power = 0.0f;
 	DebugSliderFloat("StartLength", &startLen, 0.0f, 1.0f);
-	DebugSliderFloat("Power", &power, 0.0f, 150.0f);
+	DebugSliderFloat("Power", &power, 0.0f, 500.0f);
+	if (DebugButton("Power+")) power += 20.0f;
+	if (DebugButton("Power-")) power -= 20.0f;
 	speedBlur->SetStartLength(startLen);
-	speedBlur->SetPower(power);
+	SetPower(power);
 	EndDebugWindow("SpeedBlur");
 #endif // SPEEDBLURCTRL_USE_DEBUG
 
@@ -65,12 +87,25 @@ void SpeedBlurController::Draw()
 }
 
 /**************************************
+ブラーパワーセット処理
+***************************************/
+void SpeedBlurController::SetPower(float power)
+{
+	endPower = power;
+
+	if (cntPower != 0)
+		return;
+
+	cntPower = SPEEDBLUR_TRANSITION_DURATION;
+}
+
+/**************************************
 コンストラクタ
 ***************************************/
 SpeedBlurController::SpeedBlurController()
 {
-	const float InitStartLength = 0.3f;
-	const float InitPower = 5.0f;
+	const float InitStartLength = 0.25f;
+	const float InitPower = 0.0f;
 
 	speedBlur = new SpeedBlur();
 	speedBlur->SetSurfaceSize((float)SCREEN_WIDTH, (float)SCREEN_HEIGHT);
@@ -79,9 +114,12 @@ SpeedBlurController::SpeedBlurController()
 	pDevice->CreateTexture(SCREEN_WIDTH, SCREEN_HEIGHT, 1, D3DUSAGE_RENDERTARGET, D3DFMT_X8R8G8B8, D3DPOOL_DEFAULT, &texture, 0);
 	texture->GetSurfaceLevel(0, &surface);
 
-	speedBlur->SetCenterPos(D3DXVECTOR3(0.0f, 0.0f, 150.0f));
+	speedBlur->SetCenterPos(D3DXVECTOR3(0.0f, 0.0f, 0.0f));
 	speedBlur->SetPower(InitPower);
 	speedBlur->SetStartLength(InitStartLength);
+
+	startPower = endPower = 0.0f;
+	cntPower = SPEEDBLUR_TRANSITION_DURATION;
 }
 
 /**************************************

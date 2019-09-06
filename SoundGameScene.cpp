a58e5c55ee1763b07b4b5cb2e6a9f0c9aba25805
watanabeyoshinori <wyoshinori.atpg@outlook.jp>
2019-09-06 15:50:08 +0900

    キー入力で移動した際に星のUIを回転させる処理を追加

--- SoundGameScene.cpp
+++ SoundGameScene.cpp
@@ -17,11 +17,6 @@ bool BossBGM_active;
 ***************************************/
 void SoundGameScene::Play() {
 
-
-	//サウンドの再生
-	//if (!Sound::GetInstance()->playsound) {
-		//ゲーム開始と同時に再生、フェードイン
-
 	if (CurrentGameScene == GameScene::State::Battle) {
 		//念のためボスBGMを停止しゲームBGMを再生
 		Sound::GetInstance()->SetStopSound(BOSSBGM);
@@ -37,61 +32,12 @@ void SoundGameScene::Play() {
 		Sound::GetInstance()->SetPlayBGM(BOSSBGM, true, Sound::GetInstance()->changevol / 5.0f);
 		Sound::GetInstance()->FadeIn(BOSSBGM, 5.0f, Sound::GetInstance()->changevol / 5.0f, true);
 
-
 	}
 	else if (CurrentGameScene == GameScene::State::End) {
 
 		//ボスBGMフェードアウト
 		Sound::GetInstance()->FadeOut(BOSSBGM, 5.0f, Sound::GetInstance()->changevol / 5.0f, true);
 	}
-	//}
-	//else {
-		//フェードアウトの開始
-		//Sound::GetInstance()->FadeOut(GAMEBGM, 4.5f, Sound::GetInstance()->changevol, true);
-	//}
-
-	//if (GetKeyboardTrigger(DIK_SPACE)) {
-	//	//よくあるショットSEみたいに再生
-	//	Sound::GetInstance()->SetPlaySE(TESTSE, true, Sound::GetInstance()->changevol);
-	//	//Sound::GetInstance()->ChangeSEVolume(TESTSE, 1.0f);
-	//}
-	//if (GetKeyboardTrigger(DIK_O)) {
-
-	//	Sound::GetInstance()->playsound = Sound::GetInstance()->playsound ? false : true;
-	//}
-	//if (GetKeyboardTrigger(DIK_P)) {
-	//	//trueの場合ゲーム中にSE再生
-	//	//falseの場合はポーズ中にSE再生
-	//	Sound::GetInstance()->pause = Sound::GetInstance()->pause ? false : true;
-	//	Sound::GetInstance()->SetPlaySE(ENTERSE, Sound::GetInstance()->pause, Sound::GetInstance()->changevol);
-	//	Sound::GetInstance()->SetPitchSE(ENTERSE, 0);
-	//	Sound::GetInstance()->ChangePauseSound(Sound::GetInstance()->pause);
-	//	Sound::GetInstance()->changepitch = 0;
-	//}
-
-	//if (GetKeyboardTrigger(DIK_L)) {
-	//	Sound::GetInstance()->changepitch += 100;
-	//	if (Sound::GetInstance()->changepitch > 1200) {
-	//		Sound::GetInstance()->changepitch = 1200;
-	//	}
-	//	//ポーズ中にSEが鳴らせるかテスト
-	//	Sound::GetInstance()->SetPlaySE(ENTERSE, false, Sound::GetInstance()->changevol);
-	//	//ポーズ中のみピッチの変更
-	//	Sound::GetInstance()->SetPitchSE(ENTERSE, Sound::GetInstance()->changepitch);
-
-	//}
-
-	//if (GetKeyboardTrigger(DIK_K)) {
-
-	//	Sound::GetInstance()->changepitch -= 100;
-	//	if (Sound::GetInstance()->changepitch < -1200) {
-	//		Sound::GetInstance()->changepitch = -1200;
-	//	}
-	//	//ポーズ中にSEが鳴らせるかテスト
-	//	Sound::GetInstance()->SetPlaySE(ENTERSE, false, Sound::GetInstance()->changevol);
-	//	//ポーズ中のみピッチの変更
-	//	Sound::GetInstance()->SetPitchSE(ENTERSE, Sound::GetInstance()->changepitch);
-	//}
 
 }
 
